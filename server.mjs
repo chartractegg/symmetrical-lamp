541f997606b4ecb969d2debc71a0938946ea8c97
greg cathcart <chartractegg@gmail.com>
2025-11-16 20:00:42 -0700

    Delete server.mjs

--- server.mjs
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const app = express();
-app.use(express.static(__dirname + "/public"));
-app.listen(process.env.PORT || 3000, () => console.log("ok"));
